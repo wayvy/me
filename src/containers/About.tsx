e7714508a2289b3f78ffd9cbf011b1087d6cf688
Sergey Gromov <sendtogromov@gmail.com>
2020-09-16 22:15:25 +0300

    Update About

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { createUseStyles, useTheme } from 'react-jss';
 
+import { H2 } from '../components/Typographics';
+import { Section } from '../components/Section';
+import { List } from '../components/List';
+
 const useStyles = createUseStyles((theme: Theme) => ({
   about: {},
 }));
 
 const About: React.FC = () => {
+  const skills = [
+    ['HTML'],
+    ['CSS', 'SCSS', 'JSS'],
+    ['JavaScript', 'TypeScript'],
+    ['React', 'Redux'],
+    ['Canvas'],
+    ['Webpack'],
+    ['UNIX'],
+  ];
+
   const style = {};
   const theme = useTheme();
   const classes = useStyles({ theme, ...style });
 
-  return <div className={classes.about}></div>;
+  return (
+    <main className={classes.about}>
+      <H2>Hi! I am Sergey</H2>
+      <Section name="Skills">
+        <List list={skills} />
+      </Section>
+    </main>
+  );
 };
 
 export { About };
