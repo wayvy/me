549982f33e0cdaa48f139e8091f2e52657e4b814
Sergey Gromov <sendtogromov@gmail.com>
2020-09-16 22:50:09 +0300

    Add Art

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -6,6 +6,7 @@ import { Section } from '../components/Section';
 import { List } from '../components/List';
 import { Article } from '../components/Article';
 import { Photo } from '../components/Photo';
+import { Art } from '../components/Art';
 
 const useStyles = createUseStyles((theme: Theme) => ({
   about: {},
@@ -34,15 +35,30 @@ const About: React.FC = () => {
       </Section>
       <Article>
         <Paragraph>Меня зовут Сергей Громов, я живу в Москве.</Paragraph>
-        <Paragraph>Могу сверстать любой дизайн на <em>flexbox</em>, адаптивно, кроссбраузерно, с переходами, анимациями и медиазапросами, чтоб было <em>pixel-perfect</em> и с <em>БЭМ</em>. Умею <em>SCSS</em> и <em>JSS</em>. Пользуюсь <em>Figma</em> / <em>Photoshop</em> / <em>Sketch</em>.</Paragraph>
-        <Paragraph>Занимался созданием лендингов и интернет-магазинов на <em>OpenCart</em> на фрилансе. Умею хорошо верстать, находить общий язык с заказчиками, устанавливать дэдлайны и укладываться в них. Год назад понял, что распыляюсь по задачам в разных сферах и решил углубиться в мир <em>ES6+</em>. <Link href='https://unona.su'>Последний заказ</Link>.</Paragraph>
-        <Paragraph>За этот год изучил React, Redux, TypeScript и Webpack. Сейчас разбираюсь с <em>JSS</em>, <em>Canvas</em> и <em>GraphQL</em>.</Paragraph>
-        <Paragraph><Link href='https://github.com/wayvy'>Мой github</Link>.</Paragraph>
+        <Paragraph>
+          Могу сверстать любой дизайн на <em>flexbox</em>, адаптивно, кроссбраузерно, с переходами, анимациями и
+          медиазапросами, чтоб было <em>pixel-perfect</em> и с <em>БЭМ</em>. Умею <em>SCSS</em> и <em>JSS</em>.
+          Пользуюсь <em>Figma</em> / <em>Photoshop</em> / <em>Sketch</em>.
+        </Paragraph>
+        <Paragraph>
+          Занимался созданием лендингов и интернет-магазинов на <em>OpenCart</em> на фрилансе. Умею хорошо верстать,
+          находить общий язык с заказчиками, устанавливать дэдлайны и укладываться в них. Год назад понял, что
+          распыляюсь по задачам в разных сферах и решил углубиться в мир <em>ES6+</em>.{' '}
+          <Link href="https://unona.su">Последний заказ</Link>.
+        </Paragraph>
+        <Paragraph>
+          За этот год изучил React, Redux, TypeScript и Webpack. Сейчас разбираюсь с <em>JSS</em>, <em>Canvas</em> и{' '}
+          <em>GraphQL</em>.
+        </Paragraph>
+        <Paragraph>
+          <Link href="https://github.com/wayvy">Мой github</Link>.
+        </Paragraph>
         <Paragraph>Грамотный русский;</Paragraph>
         <Paragraph>Читаю документации и статьи на английском;</Paragraph>
         <Paragraph>Люблю web, разработку и дизайн. Надеюсь найти отличную команду!</Paragraph>
-        <Photo/>
-      </Article> 
+        <Photo />
+      </Article>
+      <Art />
     </main>
   );
 };
